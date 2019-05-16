0188dee7578260c3fc94706fd58a0e2bb04651b2
Adam Fentosi <fentosi.adam@gmail.com>
2019-05-16 13:59:26 +0200

    Add Movie component

---
+++ src/components/Movie.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="movie-item">
+        <p>{{ movie.Title }}</p>
+        <p>{{ movie.Year }}</p>
+        <img :src="movie.Poster">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieItem',
+  props: ['movie']
+}
+</script>
